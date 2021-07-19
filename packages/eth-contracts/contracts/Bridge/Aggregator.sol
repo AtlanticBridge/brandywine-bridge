0c841f47a6238e0655d2de2317c32b09a4e37240
karthedew <karthedew@gmail.com>
2021-07-19 06:38:56 -0700

    added Bridge directory

---
+++ packages/eth-contracts/contracts/Bridge/Aggregator.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Aggregator {
+
+
+    constructor() public {
+        // We want to aggregate the sum of several submissions and figure
+        // out which of the values is correct.
+    }
+}
